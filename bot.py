37eb46a11c1c53044974cfe01f2c060efce269f7
Ju57u5 <2545457+ju57u5@users.noreply.github.com>
2020-07-01 23:42:45 +0200

    refactor, feat: make pylint happy, catch errors better
    - better import structure
    - remove unused imports
    - better doc string format
    - shorter, try catch in add_emote_command_handler
    - catch KeyError in add_emote_command_handler
    - replace unused vars from touples with _
    - differentiate errors in __main__ (IOError, KeyError)
    
    This is too long, but all these changes came from making pylint happy.
    And the code complexity of this bot is small anyway.

--- bot.py
+++ bot.py
@@ -3,18 +3,15 @@ TelegramTwitchEmoteBot
 
 Share Twitch emotes with a Telegram chat through a quick inline command.
 """
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, PicklePersistence
-from telegram.ext.filters import Filters
-from telegram import InlineQueryResultPhoto, InlineQueryResultCachedSticker
-import requests
 import json
 import logging
 import itertools
-import time
-from io import BytesIO
-from PIL import Image
-from uuid import uuid4
 import sys
+
+import requests
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, PicklePersistence
+from telegram.ext.filters import Filters
+from telegram import InlineQueryResultCachedSticker
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -25,8 +22,8 @@ CACHE_TIME = 10
 
 def get_emoji_query_result(emote_code, cached_emotes):
     """
-    Gets the emote information for the emote indicated by emote_code out of cached_emotes and return an
-    appropiately constructed InlineQueryResultCachedSticker.
+    Gets the emote information for the emote indicated by emote_code out of cached_emotes
+    and return an appropiately constructed InlineQueryResultCachedSticker.
 
     Returns InlineQueryResultCachedSticker(id=emote_code, sticker_file_id=file_id).
     """
@@ -37,8 +34,8 @@ def get_emoji_query_result(emote_code, cached_emotes):
 
 def create_or_get_emote_data(context):
     """
-    This will make sure that the list cached_channels and the dict emotes are initialised inside bot_data
-    before returning them.
+    This will make sure that the list cached_channels and the dict emotes are initialised
+    inside bot_data before returning them.
 
     These list and dict are used, to store the cached stickers.
 
@@ -54,18 +51,22 @@ def create_or_get_emote_data(context):
 
 def cache_stickers(context):
     """
-    This job will cache stickers by sending them to the chat indicated by chat_id inside the jobs context.
-    This is needed because Twitch emotes are generally not big enough to be included in a sticker pack.
-    Fortunatly Telegram still allows a sticker-like behavior, if you send a regular webp file that contains
-    transparency inside a private chat.
-    This file can then be send as an inline result with the returned file_id.
+    This job will cache stickers by sending them to the chat indicated by chat_id
+    inside the jobs context.
 
-    This job will therefore be scheduled to send all the stickers of a specific Twitch channel and record all
-    file_ids inside the bots data-dictonary.
+    This is needed because Twitch emotes are generally not big enough
+    to be included in a sticker pack. Fortunatly Telegram still allows a
+    sticker-like behavior, if you send a regular webp file that contains
+    transparency inside a private chat. This file can then be send as an
+    inline result with the returned file_id.
+
+    This job will therefore be scheduled to send all the stickers of a
+    specific Twitch channel and record all file_ids inside the bots data-dictonary.
 
     The webp conversion is done with the php script telegrambot.php.
 
-    When all stickers are send, the job will automatically schedule a removal from the job_queue.
+    When all stickers are send, the job will automatically schedule a
+    removal from the job_queue.
     """
     resp_emotes = context.job.context["resp_emotes"]
     if not resp_emotes:
@@ -93,45 +94,15 @@ def cache_stickers(context):
 
 def add_emote_command_handler(update, context):
     """
-    CommandHandler that adds emotes from a specific channel to the bots cache. 
-    
+    CommandHandler that adds emotes from a specific channel to the bots cache.
+
     Format: /add <channelid>.
 
     Emotes are determined with querries to the twitchemotes.com API.
     """
     try:
         channel_id = int(context.args[0])
-        cached_channels, cached_emotes = create_or_get_emote_data(context)
-
-        if channel_id in cached_channels:
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text="Channel was already added to the bot but I'm gonna check if there are new emotes.",
-            )
-
-        channel_api_url = f"https://api.twitchemotes.com/api/v4/channels/{channel_id}"
-        resp = requests.get(channel_api_url)
-        if resp.status_code == 404:
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=f"Error: channel with id {channel_id} not found.",
-            )
-            return
-        if resp.status_code != 200:
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text="Error: can't reach twitchemotes API.",
-            )
-            return
-
-        resp_emotes = resp.json()["emotes"]
-        context.job_queue.run_repeating(
-            cache_stickers,
-            interval=5,
-            context={"resp_emotes": resp_emotes, "chat_id": update.message.chat_id},
-        )
-
-        cached_channels.append(channel_id)
+        cached_channels, _ = create_or_get_emote_data(context)
 
     except ValueError:
         context.bot.send_message(
@@ -143,6 +114,47 @@ def add_emote_command_handler(update, context):
             chat_id=update.effective_chat.id, text="Missing argument: channel id."
         )
 
+    if channel_id in cached_channels:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Channel was already added to the bot, \
+                  but I'm gonna check if there are new emotes.",
+        )
+
+    channel_api_url = f"https://api.twitchemotes.com/api/v4/channels/{channel_id}"
+    resp = requests.get(channel_api_url)
+
+    if resp.status_code == 404:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Error: channel with id {channel_id} not found.",
+        )
+        return
+
+    if resp.status_code != 200:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Error: can't reach twitchemotes API.",
+        )
+        return
+
+    try:
+        resp_emotes = resp.json()["emotes"]
+    except KeyError:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Error: can't read response from twitchemotes API.",
+        )
+        return
+
+    context.job_queue.run_repeating(
+        cache_stickers,
+        interval=5,
+        context={"resp_emotes": resp_emotes, "chat_id": update.message.chat_id},
+    )
+
+    cached_channels.append(channel_id)
+
 
 def inline_query_handler(update, context):
     """
@@ -151,7 +163,7 @@ def inline_query_handler(update, context):
 
     Format @bot <emoteName>
     """
-    cached_channels, cached_emotes = create_or_get_emote_data(context)
+    _, cached_emotes = create_or_get_emote_data(context)
     query_text = update.inline_query.query
     query_id = update.inline_query.id
 
@@ -182,11 +194,18 @@ if __name__ == "__main__":
     try:
         with open("credentials.json") as config:
             key = json.load(config)["key"]
-    except Exception as e:
+    except IOError as error:
         print(
-            "Can't properly read credentials.json. Check if the file exists and it's correct format."
+            "Can't properly read credentials.json. Check if the file exists and is accessible."
         )
-        print(e)
+        print(error)
+        sys.exit()
+
+    except KeyError as error:
+        print(
+            "Can't properly read credentials.json. Check if the file is in the correct format."
+        )
+        print(error)
         sys.exit()
 
     bot_persistence = PicklePersistence(filename="bot_data", store_bot_data=True)
